31e57d9f1a30a6a9d57ec918ede10638076679f7
lennyomg <lenny.omg@gmail.com>
2015-11-04 17:12:41 +0300

    Fixed text in menu item + version 1.1.2

--- Source/VsStartupSelector/VsStartupSelector/Properties/AssemblyInfo.cs
+++ Source/VsStartupSelector/VsStartupSelector/Properties/AssemblyInfo.cs
@@ -4,9 +4,6 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("VsStartupSelector")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -19,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 
-[assembly: AssemblyVersion("1.1.1.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyVersion("1.1.2.0")]
+[assembly: AssemblyFileVersion("1.1.2.0")]
